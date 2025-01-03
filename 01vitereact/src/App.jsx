b9a287edb03b5cd62b1ac71f0e37bf18c841f9f3
riyagandhi-2023 <riyagandhi.it@gmail.com>
2025-01-03 14:49:16 -0500

    custom react added

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,13 @@
-
+import Chai from "./Chai"
 function App() {
   
 
   return (
-    <>
-      <h1>Chai aur react with vite | Riya Gandhi</h1>
-    </>
+  <>
+  <h1>Chai aur react with vite | Riya Gandhi</h1>
+  <Chai/>
+  <p>para para</p>
+  </>
   )
 }
 
